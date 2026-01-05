765f3ed049b27a8ebc06cab92fb01e16f1c6c8ac
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-01-05 21:21:07 +0530

    Add files via upload

---
+++ BinarySearch/744_Smallest_Letter_greater_than_target.java
@@ -0,0 +1,22 @@
+// Leetcode : 744 Smallest Letter Greater Than Target
+// Time Complexity O( log n)
+// Space Complexity O(1)
+
+class Solution {
+    public char nextGreatestLetter(char[] letters, char target) {
+        int low = 0 , high = letters.length -1;
+        while(low <= high)
+        {
+int mid = low + ( high - low )/2;
+if(letters[mid] <= target)
+{
+low = mid + 1;
+}
+else
+{
+    high = mid -1 ;
+}
+        }
+        return letters[low %  letters.length]; //if not get greater char
+    }
+}
