52d68871197226e976aa3ea8280354ea14fbc899
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-01-09 20:19:27 +0530

    Add files via upload

---
+++ BinarySearch/367_valid_perfect_square.java
@@ -0,0 +1,19 @@
+// Leetcode Valid Perfect Square 
+// Time Complexity O(log N)
+// Space Complexity O(1)
+class Solution {
+    public boolean isPerfectSquare(int num) {
+      long low = 1, high = num ;
+      while( low <= high )
+      {
+long mid = low + ( high - low  ) / 2 ;
+long sq = mid * mid;
+if( sq == num ) return true ;
+else if( sq < num )
+low = mid + 1;
+else
+high = mid - 1 ;
+      }
+      return false ;  
+    }
+}
