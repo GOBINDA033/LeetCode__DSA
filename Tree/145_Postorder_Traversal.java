7a2793d92f1c4c88844520c44f46f14c5ff7daab
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-01-15 21:32:07 +0530

    Add files via upload

---
+++ Tree/145_Postorder_Traversal.java
@@ -0,0 +1,36 @@
+//Leetcode: 145 Postorder Traversal
+// Time Complexity O(n)
+// Space Complexity O(h)
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+      List<Integer> ans = new ArrayList<>();
+      postorder( root ,ans);
+      return ans;  
+    }
+    private void postorder(TreeNode root , List<Integer> ans)
+    {
+        if( root == null) return;
+        // left
+        postorder( root.left , ans );
+        //right 
+        postorder( root.right , ans );
+        //root
+        ans.add( root.val );
+    }
+}
