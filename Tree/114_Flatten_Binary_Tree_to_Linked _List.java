9873a2b6af8842af246c53d7eaf3b407924a810e
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-01-23 21:32:40 +0530

    Add files via upload

---
+++ Tree/114_Flatten_Binary_Tree_to_Linked _List.java
@@ -0,0 +1,45 @@
+// Leetcode 114: Flatten Binary Tree to linked List
+// Time Compleixty O(n)
+// Space Complexity O(1)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void flatten(TreeNode root) {
+        TreeNode curr = root;
+
+        while (curr != null) {
+
+            if (curr.left != null) {
+
+                // find rightmost node of left subtree
+                TreeNode prev = curr.left;
+                while (prev.right != null) {
+                    prev = prev.right;
+                }
+
+                // connect right subtree
+                prev.right = curr.right;
+
+                // move left subtree to right
+                curr.right = curr.left;
+                curr.left = null;
+            }
+
+            curr = curr.right;
+        }
+    }
+}
+
