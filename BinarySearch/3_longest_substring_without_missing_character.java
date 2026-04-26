6a4004a30f6949e4ea9cea38f52d78d84ddf0225
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-04-26 23:17:22 +0530

    Add files via upload

---
+++ BinarySearch/3_longest_substring_without_missing_character.java
@@ -0,0 +1,24 @@
+// 3. Longest Substring Without Missing Character3
+// Time complexity: O(n)
+// Spcae Complexity: O(n)
+
+
+import java.util.*;
+
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> set = new HashSet<>();
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (set.contains(s.charAt(right))) {
+                set.remove(s.charAt(left));
+                left++;
+            }
+            set.add(s.charAt(right));
+            maxLength = Math.max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+}
