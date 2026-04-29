1832a6b7a191c1c621f083fb3e4ec0c2d035c6bb
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-04-29 23:39:10 +0530

    Add files via upload

---
+++ Array/11_Container_with_most_water.java
@@ -0,0 +1,27 @@
+// Container with most water
+// I am using two pointer approach to solve this problem
+
+//Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+    public int maxArea(int[] height) {
+        int i = 0, j = height.length-1, ans =0;
+        while( i<j )
+        {
+            int area = (j-i)*Math.min(height[i],height[j]);
+            ans =Math.max(ans,area);
+            if( height[i]< height[j] )
+            {
+i++;
+            }
+            else
+            {
+                j--;
+            }
+
+        }
+        return ans;
+    }
+}
+
