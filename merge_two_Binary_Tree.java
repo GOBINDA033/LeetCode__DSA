a18f0b97a65770d79e29d816ac623bb170d31bdb
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-05-01 23:02:24 +0530

    Update merge_two_Binary_Tree.java

--- merge_two_Binary_Tree.java
+++ merge_two_Binary_Tree.java
@@ -17,7 +17,7 @@
  *     }
  * }
  */
-class Solution {
+class Solution { 
     public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
        if( root1 == null ) return root2;
        if( root2 == null ) return root1;
