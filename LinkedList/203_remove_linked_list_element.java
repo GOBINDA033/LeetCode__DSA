b07168167bcd7784d610206c162ccd118be27856
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-05-03 22:33:10 +0530

    Add files via upload

---
+++ LinkedList/203_remove_linked_list_element.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+
+
+
+// 203. Remove linked List Element
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+
+        ListNode current = dummy;
+
+        while (current.next != null) {
+            if (current.next.val == val) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return dummy.next;
+    }
+}
