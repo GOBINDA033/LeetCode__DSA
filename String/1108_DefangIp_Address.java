ef82ac5d5aa1995065095aa13e3a2e9d29ae7261
Gobinda <114664223+GOBINDA033@users.noreply.github.com>
2026-05-08 21:59:55 +0530

    Add files via upload

---
+++ String/1108_DefangIp_Address.java
@@ -0,0 +1,8 @@
+// Defang IP Address
+// Time complexity: O(n)
+// Space Complexity: O(n)
+class Solution {
+    public String defangIPaddr(String address) {
+        return address.replace(".", "[.]");
+    }
+}
